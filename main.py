2c5ec64f52b3208b53e3ef1c10381f3e07de2391
Jagadrubygad <41937681+jagadyudha@users.noreply.github.com>
2019-07-27 23:29:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,35 @@
-#!/usr/bin/python
-### coded by jagad ###
-
 from bs4 import BeautifulSoup
 import urllib
 import requests
 
-print("")
-print("    ###c0ded by jagad###        $$\                                       ")
-print("  ------------------------      $$ |                                      ")
-print(" $$$$$$\   $$$$$$$\ $$$$$$\   $$$$$$\    $$$$$$\   $$$$$$\  $$$$$$\$$$$\  ")
-print("$$  __$$\ $$  _____|\____$$\  \_$$  _|  $$  __$$\  \____$$\ $$  _$$  _$$\ ")
-print("$$ /  $$ |$$ /      $$$$$$$ |   $$ |    $$$$$$$$ | $$$$$$$ |$$ / $$ / $$ |")
-print("$$ |  $$ |$$ |     $$  __$$ |   $$ |$$\ $$   ____|$$  __$$ |$$ | $$ | $$ |")
-print("\$$$$$$$ |\$$$$$$$\\$$$$$$$ |$$\\$$$$  |\$$$$$$$\ \$$$$$$$ |$$ | $$ | $$ |")
-print(" \____$$ | \_______|\_______|\__|\____/  \_______| \_______|\__| \__| \__|")
-print("$$\   $$ |                                                                ")
-print("\$$$$$$  |                                                                ")
-print(" \______/                                                                 ")
-print("")
+# Code By : Jagad Yudha
+# Thanks To : Me xD
 
-dork = raw_input("(Input Dork!) : ")
-print ("")
-pages_number = []
-pages_number.append(0)
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-for pgs in range(1, 1000):
-	if pgs % 10 == 0:
-		pages_number.append(pgs)
+pages_data = [0]
 
-encoded = urllib.quote(dork)
+def get_dork(dork):
+  dork_quote = urllib.quote(dork)
+  for i in pages_data:
+    dork_page = str(i)
+    dork_url = "https://www.google.com/search?q=" +dork +"&start=" +dork_page
+    dork_req = requests.get(dork_url, headers=headers).text
+    dork_soup = BeautifulSoup(dork_req, "html.parser")
+    for j in dork_soup.find_all('div', attrs={'class': 'g'}):
+      get_link = j.find('a', href=True)
+      get_link_href = get_link['href']
+      print  get_link_href
+    
+def get_pages(pages):
+  pages_str = pages+"0"
+  pages_int = int(pages_str)
+  for i in range(1,pages_int):
+    if i % 10 == 0:
+      pages_data.append(i)
 
-try:
-	for p in pages_number:
-		pages = str(p)
-		url_1 = "https://www.google.com/search?q=" +encoded +"&start=" +pages
-		url_2 = requests.get(url_1)
-		wow = BeautifulSoup(url_2.text, "html.parser")
-		for i in wow.findAll('cite'):
-			print ("Link grabbed dude !!!")
-			output = i.text
-			myfile = open('output.txt', 'a')
-			myfile.write(output + "\n")
+dork = raw_input("Input Dork :")
+pages = raw_input("Input Pages :")
 
-except:
-	print ("")
+get_dork(dork)
+get_pages(pages)
