e1aff29dbb1c61c8476ac278ab681ea1575fb808
Jagad Yudha Awali <41937681+jagadyudha@users.noreply.github.com>
2021-06-21 09:05:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from bs4 import BeautifulSoup
 import urllib
 import requests
 
-# Code By : Jagad Yudha
-# Thanks To : Me xD
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
