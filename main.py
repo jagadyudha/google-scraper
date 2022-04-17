9e9552dcab5c93012157c7940cd460739ef1966b
unknown <jagadyudhaawali@gmail.com>
2022-04-17 19:55:05 +0700

    refactor: remove unused variable

--- main.py
+++ main.py
@@ -1,35 +1,27 @@
 from bs4 import BeautifulSoup
-import urllib
 import requests
 
 pages_data = [0]
 
-def get_pages(pages):
-    pages_str = pages+"0"
-    pages_int = int(pages_str)
-    for i in range(1, pages_int):
+def get_pages(params):
+    pages = int(params+"0")
+    for i in range(1, pages):
         if i % 10 == 0:
             pages_data.append(i)
 
 
-def get_dork(dork):
+def get_dork(params):
     headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36 OPR/58.0.3135.79'}
-    print("\n\n")
     for i in pages_data:
-        dork_page = str(i)
-        dork_url = "https://www.google.com/search?q=" + \
-            dork + "&start=" + dork_page
-        dork_req = requests.get(dork_url, headers=headers).text
-        dork_soup = BeautifulSoup(dork_req, "html.parser")
-        for j in dork_soup.find_all('div', attrs={'class': 'g'}):
-            get_link = j.find('a', href=True)
-            get_link_href = get_link['href']
-            print(get_link_href)
-
+        url = "https://www.google.com/search?q=" + \
+            params + "&start=" + str(i)
+        dork = BeautifulSoup(requests.get(url, headers=headers).text, "html.parser")
+        for j in dork.find_all('div', attrs={'class': 'g'}):
+            result = j.find('a', href=True)['href']
+            print(result)
 
 pages = input("Input Pages : ")
 dork = input("Input Data : ")
-
 get_pages(pages)
 get_dork(dork)
